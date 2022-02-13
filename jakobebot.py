01ce57312e8995953a871f82d6423bda46205aad
Jacob Zhang <Jzhang5850@gmail.com>
2022-02-12 23:03:27 -0500

    Set ativity and line formatting

--- jakobebot.py
+++ jakobebot.py
@@ -3,8 +3,10 @@ import json
 import random
 import mytoken
 
-client = discord.Client()
+client = discord.Client(activity=discord.Game(name='with your mom'))
+
 prefix = "jakobe"
+
 with open("quizdb.json", encoding="utf8") as file:
     y = json.load(file)
 
@@ -15,6 +17,7 @@ with open("users.json", encoding="utf8") as file:
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -22,8 +25,10 @@ async def on_message(message):
 
     if message.content.startswith(prefix + ' ping'):
         await message.channel.send(':face_with_symbols_over_mouth:')
+
     if message.content.startswith('cope'):
         await message.channel.send('cope')
+
     if message.content.startswith(prefix + ' praise'):
         if str(message.author.id) in z.keys():
             z[str(message.author.id)] += 1
@@ -32,6 +37,7 @@ async def on_message(message):
         with open("users.json", "w") as outfile:
             json.dump(z, outfile)
         await message.channel.send("thank you, I am indeed a great bot" + "\n" + "your jakobe-credit-score:tm: is " + str(z[str(message.author.id)]))
+
     if message.content.startswith(prefix + ' quiz'):
         channel = message.channel
         quiz = y[random.randrange(1,100)]
